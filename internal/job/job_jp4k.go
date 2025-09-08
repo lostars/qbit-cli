081fdc0958d4af0247d3cf88ce382c0704e25a48
lostars <746320542jn@gmail.com>
2025-09-08 14:51:28 +0800

    feat: add parallelism to jp4k

--- internal/job/job_jp4k.go
+++ internal/job/job_jp4k.go
@@ -11,6 +11,7 @@ import (
 	"qbit-cli/internal/config"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -48,6 +49,8 @@ func (j *JP4K) RunCommand() *cobra.Command {
 		premiereBefore, premiereAfter string
 		extraCodes                    []string
 		excludeUrls                   []string
+		maxParallelism                int
+		parentId                      int
 	)
 
 	saveCategory := c.FlagsProperty[string]{Flag: "save-category", Register: &c.TorrentCategoryFlagRegister{}}
@@ -68,6 +71,10 @@ this option will find all jp video's 4k version which is filter by extra jp code
 
 	cmd.Flags().StringSliceVar(&excludeUrls, "exclude-urls", []string{}, `exclude certain search urls, using contains`)
 
+	cmd.Flags().IntVar(&maxParallelism, "max-parallelism", 2, "max parallelism")
+
+	cmd.Flags().IntVar(&parentId, "parent-id", 0, "emby item parent id")
+
 	// register completion
 	saveCategory.RegisterCompletion(cmd)
 	plugins.RegisterCompletion(cmd)
@@ -83,6 +90,10 @@ this option will find all jp video's 4k version which is filter by extra jp code
 		if premiereAfter != "" {
 			searchParams.Add("MinPremiereDate", premiereAfter)
 		}
+		if parentId > 0 {
+			searchParams.Add("IncludeItemTypes", "Movie")
+			searchParams.Add("ParentId", strconv.Itoa(parentId))
+		}
 
 		items := fourKItems(searchParams)
 		items = append(items, searchExtraCodes(extraCodes)...)
@@ -91,40 +102,53 @@ this option will find all jp video's 4k version which is filter by extra jp code
 			return nil
 		}
 
+		var wg sync.WaitGroup
+		var mu sync.Mutex
 		data := make([]*api.SearchDetail, 0, len(items)*2)
-		for _, item := range items {
-			// parse code from name
-			matches := JPCodeRegex.FindStringSubmatch(item.Name)
-			if len(matches) < 2 {
-				continue
-			}
-			jpCode := matches[1]
-			fmt.Printf("searching 4k version of %s...\n", jpCode)
-
-			params := url.Values{
-				"pattern":  {jpCode},
-				"category": {"all"},
-			}
-			if plugins.Value != "" {
-				params.Set("plugins", plugins.Value)
-			}
-			result, err := api.SearchStart(params)
-			if err != nil {
-				fmt.Printf("search start error: %s\n", err)
-				continue
-			}
-			results, err := api.SearchDetails(1*time.Second, result.ID)
-			if err != nil {
-				fmt.Printf("search details error: %s\n", err)
-				continue
-			}
-
-			for _, r := range results {
-				if JP4KRegex.MatchString(r.FileName) {
-					data = append(data, r)
+		limit := make(chan struct{}, maxParallelism)
+		for i := range items {
+			wg.Add(1)
+			go func(i int) {
+				defer wg.Done()
+				limit <- struct{}{}
+				item := items[i]
+				// parse code from name
+				matches := JPCodeRegex.FindStringSubmatch(item.Name)
+				if len(matches) < 2 {
+					return
 				}
-			}
+				jpCode := matches[1]
+				fmt.Printf("searching 4k version of %s...\n", jpCode)
+
+				params := url.Values{
+					"pattern":  {jpCode},
+					"category": {"all"},
+				}
+				if plugins.Value != "" {
+					params.Set("plugins", plugins.Value)
+				}
+				result, err := api.SearchStart(params)
+				if err != nil {
+					fmt.Printf("search start error: %s\n", err)
+					return
+				}
+				results, err := api.SearchDetails(1*time.Second, result.ID)
+				if err != nil {
+					fmt.Printf("search details error: %s\n", err)
+					return
+				}
+
+				for _, r := range results {
+					if JP4KRegex.MatchString(r.FileName) {
+						mu.Lock()
+						data = append(data, r)
+						mu.Unlock()
+					}
+				}
+				<-limit
+			}(i)
 		}
+		wg.Wait()
 
 		if len(data) > 0 {
 			urls := make([]string, 0, len(data))
@@ -187,20 +211,22 @@ func searchExtraCodes(codes []string) []*api.EmbyItem {
 
 func fourKItems(searchParams url.Values) []*api.EmbyItem {
 	// search 4k tag first
-	tagParams := url.Values{
-		"SearchTerm":       {"4k"},
-		"IncludeItemTypes": {"genre"},
-		"Recursive":        {"true"},
+	if searchParams.Get("ParentId") == "" {
+		tagParams := url.Values{
+			"SearchTerm":       {"4k"},
+			"IncludeItemTypes": {"genre"},
+			"Recursive":        {"true"},
+		}
+		tags, err := emby.Items(tagParams)
+		if err != nil {
+			return nil
+		}
+		if len(tags.Items) < 1 {
+			return nil
+		}
+		tag := tags.Items[0].ID
+		searchParams.Add("GenreIds", tag)
 	}
-	tags, err := emby.Items(tagParams)
-	if err != nil {
-		return nil
-	}
-	if len(tags.Items) < 1 {
-		return nil
-	}
-	tag := tags.Items[0].ID
-	searchParams.Add("GenreIds", tag)
 
 	items, err := emby.Items(searchParams)
 	if err != nil {

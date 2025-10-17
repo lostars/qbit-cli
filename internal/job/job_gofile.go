d2e65edbdb249d9cdc09f4ea1b2816e0c5dbffd2
lostars <746320542jn@gmail.com>
2025-10-17 14:58:58 +0800

    add traffic statistics to gofile

--- internal/job/job_gofile.go
+++ internal/job/job_gofile.go
@@ -18,6 +18,7 @@ import (
 	"qbit-cli/pkg/utils"
 	"regexp"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -48,6 +49,100 @@ func init() {
 	api.RegisterJob(&Gofile{})
 }
 
+func GofileTrafficCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "traffic",
+		Short: "show your ip traffic in 1 month",
+		Args:  cobra.NoArgs,
+	}
+
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		g := Gofile{
+			client: buildGofileHttpClient(),
+		}
+		err := g.setAuth()
+		if err != nil {
+			return err
+		}
+
+		traffic, err := g.getTraffic()
+		if err != nil {
+			return err
+		}
+		if traffic.Status != "ok" || traffic.Data.IpTraffic == nil {
+			return errors.New("traffic return " + traffic.Status)
+		}
+
+		var start = time.Now().AddDate(0, -1, 0)
+		var used uint64 = 0
+		for yearNum, year := range traffic.Data.IpTraffic {
+			y, err := strconv.ParseInt(yearNum, 10, 64)
+			if err != nil {
+				continue
+			}
+			for monthNum, month := range year {
+				m, err := strconv.ParseInt(monthNum, 10, 64)
+				if err != nil {
+					continue
+				}
+				for dayNum, day := range month {
+					d, err := strconv.ParseInt(dayNum, 10, 64)
+					if err != nil {
+						continue
+					}
+					var date = time.Date(int(y), time.Month(m), int(d), 0, 0, 0, 0, time.Local)
+					if date.After(start) {
+						log.Printf("%s: %s\n", date.Format("2006-01-02"), utils.FormatFileSizeAuto(day, 1))
+						used += day
+					}
+				}
+			}
+		}
+
+		fmt.Println(traffic.Data.IpInfo.IP)
+		fmt.Printf("Total IP Traffic: %s\n", utils.FormatFileSizeAuto(used, 1))
+		fmt.Printf("Total IP Traffic In Bytes: %v\n", used)
+
+		return nil
+	}
+
+	return cmd
+}
+
+func buildGofileHttpClient() *http.Client {
+	return &http.Client{Timeout: time.Minute * 1}
+}
+
+func (r *Gofile) getTraffic() (*GofileTrafficResp, error) {
+	var trafficApi = "https://api.gofile.io/accounts/website"
+	req, _ := http.NewRequest(http.MethodGet, trafficApi, nil)
+	req.Header.Set("Authorization", "Bearer "+r.token)
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var traffic GofileTrafficResp
+	err = json.NewDecoder(resp.Body).Decode(&traffic)
+	if err != nil {
+		return nil, err
+	}
+
+	return &traffic, nil
+}
+
+type GofileTrafficResp struct {
+	Status string `json:"status"`
+	Data   struct {
+		IpTraffic map[string]map[string]map[string]uint64 `json:"ipTraffic"`
+		IpInfo    struct {
+			IP   string `json:"_id"`
+			CIDR string `json:"cidr"`
+		} `json:"ipinfo"`
+	} `json:"data"`
+}
+
 func (r *Gofile) RunCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "gofile <url>",
@@ -57,6 +152,8 @@ For free accounts, traffic is counted by IP address.
 If you're using a shared IP address, this may include additional traffic beyond your own usage.`,
 		Args: cobra.ExactArgs(1),
 	}
+
+	cmd.AddCommand(GofileTrafficCmd())
 
 	cmd.Flags().StringVar(&gofile.savePath, "save-path", "", "file save path, default is current working directory")
 	cmd.Flags().StringVarP(&gofile.password, "password", "p", "", "url share password")
@@ -77,7 +174,7 @@ If you're using a shared IP address, this may include additional traffic beyond 
 			return errors.New("invalid url")
 		}
 		gofile.url = l
-		gofile.client = &http.Client{Timeout: time.Minute * 60}
+		gofile.client = buildGofileHttpClient()
 
 		/*
 
@@ -88,7 +185,10 @@ If you're using a shared IP address, this may include additional traffic beyond 
 
 		*/
 
-		gofile.setAuth()
+		err = gofile.setAuth()
+		if err != nil {
+			return err
+		}
 
 		var contents = gofile.getFiles()
 		if contents == nil {
@@ -104,6 +204,7 @@ If you're using a shared IP address, this may include additional traffic beyond 
 		downloader := utils.HttpFileDownloader{
 			SavePath:  gofile.savePath,
 			MaxWorker: gofile.maxWorker,
+			Client:    gofile.client,
 			Headers: map[string]string{
 				"Authorization": "Bearer " + gofile.token,
 			},
@@ -137,29 +238,26 @@ If you're using a shared IP address, this may include additional traffic beyond 
 	return cmd
 }
 
-func (r *Gofile) setAuth() {
+func (r *Gofile) setAuth() error {
 
 	// set wt
 	var wtJS = "https://gofile.io/dist/js/global.js"
 	req, _ := http.NewRequest(http.MethodGet, wtJS, nil)
 	resp, err := r.client.Do(req)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
 	content := string(body)
 
 	match := regexp.MustCompile(`appdata\.wt.*"(.*)"`).FindStringSubmatch(content)
 	if len(match) <= 1 {
-		log.Println("wt failed to get")
-		return
+		return errors.New("wt get failed")
 	}
 	r.wt = match[1]
 	log.Printf("wt: %s\n", r.wt)
@@ -169,20 +267,20 @@ func (r *Gofile) setAuth() {
 	tokenReq, _ := http.NewRequest(http.MethodPost, tokenUrl, nil)
 	resp, err = r.client.Do(tokenReq)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
 	defer resp.Body.Close()
 	var gofileToken GofileTokenResp
 	err = json.NewDecoder(resp.Body).Decode(&gofileToken)
 	if err != nil {
 		log.Println(err)
-		return
+		return err
 	}
 	r.token = gofileToken.Data.Token
 	if r.token == "" {
-		log.Println("token get failed")
+		return errors.New("token get failed")
 	}
+	return nil
 }
 
 type GofileTokenResp struct {
